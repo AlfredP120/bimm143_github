b20a3711196a0fef75a47b39d56af0cbb002b473
Alfred Phyo <aphyo@ucsd.edu>
2026-03-12 11:09:29 -0700

    add all other projects

---
+++ class04/class04.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4 R Intro"
+#' author: "Alfred Phyo"
+#' date: "Jan 15th, 2026"
+#' ---
+
+x <- 1:50
+plot(x,sin(x))
+plot(x,sin(x),typ="l",col="dark green", lwd = 2)
+log(10,base = 10)
